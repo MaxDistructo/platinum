c8777986554e660a0281c4bee8948020ac21a3cd
Mican Vollan <mvollan01@outlook.com>
2022-09-02 16:43:18 -0600

    BotMain: Add new commands for managing roles - As a function of this bot, we want to be able to push out roles a bit   easier. This adds 4 commands focusing on this. The first 2 variants   are test modes of the primary mode so that you can see how many   members it will update before running it.

--- src/kotlin/io/github/m_vollan/omega/bot/BotMain.kt
+++ src/kotlin/io/github/m_vollan/omega/bot/BotMain.kt
@@ -2,6 +2,17 @@ package io.github.m_vollan.omega.bot
 
 import ch.qos.logback.classic.Logger
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
+import net.dv8tion.jda.api.interactions.commands.build.Commands
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationMap
+import net.dv8tion.jda.api.interactions.commands.privileges.IntegrationPrivilege
+import net.dv8tion.jda.api.requests.RestAction
+import net.dv8tion.jda.api.requests.restaction.CommandEditAction
 
 object BotMain {
     val bot_name = "Omega"
@@ -12,6 +23,44 @@ object BotMain {
         logger.info("Adding slash commands")
         jda.addEventListener(SlashCommandListenerAdapter())
         jda.upsertCommand("ping", "Ping the bot").queue()
+        jda.upsertCommand(Commands.slash("echo", "Repeats what you said")
+            .addOption(OptionType.STRING, "message", "the message to say", true, false)
+        ).complete()
+        jda.upsertCommand(Commands.slash("give_role", "Gives a role to a user")
+            .setGuildOnly(true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+            .addOption(OptionType.ROLE,"role","The role to give", true, false)
+            .addOption(OptionType.USER, "user", "The user to give a role to", true, false)
+        ).complete()
+        jda.upsertCommand(Commands.slash("give_all_role", "Give all members a role")
+            .setGuildOnly(true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+            .addOption(OptionType.ROLE, "role", "The role to give", true, false)
+        ).complete()
+        jda.upsertCommand(Commands.slash("give_all_role_restricted", "Give all members a role if they don't have another role")
+            .setGuildOnly(true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+            .addOption(OptionType.ROLE, "role", "The role to give out", true, false)
+            .addOption(OptionType.ROLE, "role2", "1st restriction", true, false)
+            .addOption(OptionType.ROLE, "role3", "2nd restriction", false, false)
+            .addOption(OptionType.ROLE, "role4", "3rd restriction", false, false)
+            .addOption(OptionType.ROLE, "role5", "4th restriction", false, false)
+        ).complete()
+        jda.upsertCommand(Commands.slash("test_give_all_role", "Give all members a role")
+            .setGuildOnly(true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+            .addOption(OptionType.ROLE, "role", "The role to give", true, false)
+        ).complete()
+        jda.upsertCommand(Commands.slash("test_give_all_role_restricted", "Give all members a role if they don't have another role")
+            .setGuildOnly(true)
+            .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES))
+            .addOption(OptionType.ROLE, "role", "The role to give out", true, false)
+            .addOption(OptionType.ROLE, "role2", "1st restriction", true, false)
+            .addOption(OptionType.ROLE, "role3", "2nd restriction", false, false)
+            .addOption(OptionType.ROLE, "role4", "3rd restriction", false, false)
+            .addOption(OptionType.ROLE, "role5", "4th restriction", false, false)
+        ).complete()
+        logger.info("Finished adding slash commands")
     }
 
 }
--- src/kotlin/io/github/m_vollan/omega/bot/SlashCommandListenerAdapter.kt
+++ src/kotlin/io/github/m_vollan/omega/bot/SlashCommandListenerAdapter.kt
@@ -1,5 +1,9 @@
 package io.github.m_vollan.omega.bot
 
+import kotlinx.coroutines.*
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
@@ -10,8 +14,93 @@ class SlashCommandListenerAdapter: ListenerAdapter() {
             "ping"-> event.reply("Pong!").setEphemeral(true).flatMap{
                 event.hook.editOriginalFormat("Pong: %d ms", System.currentTimeMillis() - time)} // then edit original
                 .queue()
+            "echo" -> event.reply(event.getOption("message")!!.asString).queue()
+            "give_role" -> giveRole(event)
+            "give_all_role" -> giveRoleBulk(event, false)
+            "give_all_role_restricted" -> giveRoleBulk(event,false)
+            "test_give_all_role" -> giveRoleBulk(event, true)
+            "test_give_all_role_restricted" -> giveRoleBulk(event,true)
             else -> println("Command not found")
         }
+    }
+    fun giveRole(event: SlashCommandInteractionEvent)
+    {
+        event.deferReply().queue()
+        val role = event.getOption("role")!!.asRole
+        val user = event.getOption("user")!!.asUser
+        val guild = event.guild
+        guild!!.addRoleToMember(user, role).complete()
+        event.hook.sendMessage("Done").queue()
+    }
+    class RoleFindThread(val guild: Guild, val roleList: List<Role>, val restrictionList: MutableList<Member>): Thread()
+    {
+        //This is NOT memory efficient but easier to list for the finish of this thread being once we add all
+        //the members to the shared list
+        override fun run() {
+            val restrictions: MutableList<Member> = mutableListOf()
+            for(role2 in roleList) {
+                BotMain.logger.debug("Finding members with role: ${role2.name}")
+                guild.findMembers { m -> m.roles.contains(role2) }.onSuccess{l -> restrictions.addAll(l)}.get()
+            }
+            restrictionList.addAll(restrictions)
+        }
+    }
 
+    class AddRoleThread(val event: SlashCommandInteractionEvent, val guild: Guild, val role: Role, val restrictionList: MutableList<Member>, val testMode: Boolean): Thread()
+    {
+        override fun run()
+        {
+            while(restrictionList.size <= 0){
+                BotMain.logger.debug("Sleeping 100ms, waiting for completion of finding members")
+                sleep(100)
+            }
+            var numAdditions = 0
+            //For each member, if they are not in the restricted list, add the role
+            BotMain.logger.debug("Looking for members without the roles. ${restrictionList.size} members have the role.")
+            for(member in guild.members)
+            {
+                //BotMain.logger.debug("Checking if ${member.effectiveName} needs ${role.name}")
+                if(!restrictionList.contains(member))
+                {
+                    if(!testMode) {
+
+                        guild.addRoleToMember(member, role).queue()
+                    }
+                    numAdditions++
+                }
+            }
+            if(testMode){
+                event.hook.sendMessage("${role.name} would be rolled out to $numAdditions members").queue()
+            }
+            else {
+                event.hook.sendMessage("${role.name} is being rolled out to $numAdditions members").queue()
+            }
+        }
+    }
+
+    fun giveRoleBulk(event: SlashCommandInteractionEvent, testMode: Boolean)
+    {
+        event.deferReply().queue()
+        val role = event.getOption("role")!!.asRole
+        val guild = event.guild!!
+        val otherRoles: MutableList<Role> = mutableListOf()
+        //Try to get each role one by one. This shouldn't fail until we get all the restrictions in the list
+        try{
+            otherRoles.add(event.getOption("role2")!!.asRole)
+            otherRoles.add(event.getOption("role3")!!.asRole)
+            otherRoles.add(event.getOption("role4")!!.asRole)
+            otherRoles.add(event.getOption("role5")!!.asRole)
+        }
+        catch(_: Exception){}
+
+        //Also add members of the new role to the restricted list so that we don't double give out
+        otherRoles.add(role)
+
+        val membersOfRestricted: MutableList<Member> = mutableListOf()
+
+        //Start finding all the members that do NOT need the role or have been restricted from getting it
+        RoleFindThread(guild, otherRoles.toList(), membersOfRestricted).start()
+        //Start another thread to add the roles to all the members
+        AddRoleThread(event, guild, role, membersOfRestricted, testMode).start()
     }
 }
