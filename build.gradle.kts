dcab1fb05252393f359b6510ecd0ac4678862693
MaxDistructo <MaxDistructo@users.noreply.github.com>
2024-12-14 08:46:55 -0600

    Manually do updates due to Kotlin v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,18 +6,11 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 plugins {
-    kotlin("jvm") version "1.9.25"
-    //id("com.github.johnrengelman.shadow") version "7.1.2"
-    //id "com.github.ben-manes.versions" version "0.20.0"
-    //id 'com.sedmelluq.jdaction' version '1.0.2'
-    //id("org.springframework.boot") version "3.2.2"
-    //id("io.spring.dependency-management") version "1.1.4"
-    //kotlin("plugin.spring") version "1.9.22"
-    //kotlin("plugin.jpa") version "1.9.22"
+    kotlin("jvm") version "2.1.0"
 }
 group = "io.dedyn.engineermantra"
 
@@ -33,21 +26,21 @@ repositories {
 
 // In this section you declare the dependencies for your production and test code
 dependencies {
-    implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib", version = "1.9.25")
-    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.8.1")
+    implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib", version = "2.1.0")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.9.0")
     implementation(group = "org.json", name = "json", version = "20240303")
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.5.12")
     //Due to bugs in the latest beta, we want to pull the latest commit from Jitpack instead of Maven.
     //implementation(group = "com.github.discord-jda", name = "JDA", version = "79b1b560b1")
-    implementation(group = "net.dv8tion", name = "JDA", version = "5.1.0")
+    implementation(group = "net.dv8tion", name = "JDA", version = "5.2.1")
     implementation(group = "club.minnced", name = "discord-webhooks", version = "0.8.4")
-    implementation(group = "org.mariadb.jdbc", name = "mariadb-java-client", version = "3.5.0")
-    implementation(group = "net.java.dev.jna", name = "jna", version = "5.14.0")
-    implementation(group = "net.java.dev.jna", name = "jna-platform", version = "5.14.0")
+    implementation(group = "org.mariadb.jdbc", name = "mariadb-java-client", version = "3.5.1")
+    implementation(group = "net.java.dev.jna", name = "jna", version = "5.15.0")
+    implementation(group = "net.java.dev.jna", name = "jna-platform", version = "5.15.0")
     implementation(group = "net.sourceforge.tess4j", name = "tess4j", version = "5.13.0")
     implementation(group = "edu.cmu.sphinx", name = "sphinx4-core", version = "5prealpha-SNAPSHOT")
     implementation(group = "edu.cmu.sphinx", name = "sphinx4-data", version = "5prealpha-SNAPSHOT")
-    implementation("org.mariuszgromada.math:MathParser.org-mXparser:6.0.0")
+    implementation("org.mariuszgromada.math:MathParser.org-mXparser:6.1.0")
     /**
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
